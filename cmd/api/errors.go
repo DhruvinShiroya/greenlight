daeed28ca0e9daa6f45935e2aba432386c2214e6
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-23 16:31:04 -0400

    create custom error response writer -logError method for console error logging -errorResponse for accepting generic message

---
+++ cmd/api/errors.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+
+	// write the resposne using writeJSON() helper
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+
+}
+
+// the serverErrorResponse() method will be used when our application encounters an
+// unexpected error at runtime, it logs the detailed error message , then used the
+// errorResponse() helper to send 500 internal server error status code and JSON response
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// the notFoundResponse() method will send reponse code 404 not found response to the request
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the request resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// the methodNotAllowedResponse() method will be used to send a 405 method not allowed
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
