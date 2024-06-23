c831e8568ad7b2dd15c10d502e0be534aa4ba3ad
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-23 16:05:32 -0400

    **Changes** - use HTTP router package to handle advance routing since "net/http" router doesn't have
    - create routes to handle incoming request for movie model
    - assign custom response to router
    - register function for methods "GET", "POST", "PUT", "DELETE"
    - also handle query parameter like id for movies

---
+++ cmd/api/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// initialize new http router
+	router := httprouter.New()
+
+	// convert notFoundResponse() to be used with httprouter http.Handler adapter
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	// register the relevant methods , url patter and handler functions for our
+	// endpoints using handlefunc() method
+	// http.MethodGet and http.MethodPost is constant
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+	// return the httprouter instance
+	return router
+
+}
