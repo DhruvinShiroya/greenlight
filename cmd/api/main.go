1caff06ffac82457eb98bc7478774dc69d7407d7
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-23 16:24:05 -0400

    create db struct to handle database connection string, and advance connection config like maxOpenConns, maxIdleConns, maxIdleTime for better optimizing application response for db pool connection.
    provide necessary config, establish database connection to handle database related query
    
    note
    - environment variable  GREENLIGHT_DB_DSN for database connection string
    - for testing provide env variable[example]{GREENLIGHT_DB_DSN='postgres://user:password@address/database?sslmode=disable}

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/DhruvinShiroya/greenlight/internal/data"
+	_ "github.com/lib/pq"
 )
 
 // build version
@@ -16,6 +19,12 @@ const version = "1.0.0"
 type Config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 // define the application struct to hold dependencies for our HTTP handlers , helpers
@@ -35,11 +44,29 @@ func main() {
 	// flag are provided
 	flag.IntVar(&config.port, "port", 4000, "api server port")
 	flag.StringVar(&config.env, "env", "development", "Environment (development|staging|production)")
+	// Read the DSN value from the db-dsn command-line flag into the config struct.
+	flag.StringVar(&config.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	// Read the db connection pool maxOpenConns, maxIdleConns , maxIdleTime
+	flag.IntVar(&config.db.maxOpenConns, "db-max-open-conns", 25, "Postgres max open connection")
+	flag.IntVar(&config.db.maxIdleConns, "db-max-idle-conns", 25, "Postgres max idle connection")
+	flag.StringVar(&config.db.maxIdleTime, "db-max-idle-time", "15m", "Postgres max connection idle connection")
 	flag.Parse()
 
 	// initialize the new logger which writes to the out stream
 	// prefixed with current date and time
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// call openDB() function to create connection pool
+	db, err := openDb(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// always close the the db pool before the main function is close
+	defer db.Close()
+
+	// print if the connection was established successfully
+	fmt.Println("database connection is established")
 
 	// declare the instance of the application struct
 	// provide the config and logger instance
@@ -68,4 +95,35 @@ func main() {
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
 
+}
+
+func openDb(cfg Config) (*sql.DB, error) {
+	// use sql.Open() to create connection pool
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	// set the max connection , setmaxidelconnns, setMaxIdletime
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// parse the "<time>m" value for time.Duration
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+	// create a context with 5-second timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// use PingContext() to establish connection
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// return sql.DB
+	return db, nil
 }
