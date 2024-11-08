c42478203ff0630a586bf9f533a06164ecc363c7
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-11-08 00:59:32 -0500

    create new model for database operation on movie and users

--- internal/data/model.go
+++ internal/data/model.go
@@ -8,30 +8,29 @@ import (
 // custom error for get method when record isn't found
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 // you can add as many model as we want in our service
 // add movie model to struct model
 // for unit testing the any models we will replace the modles struct with interface
 type Models struct {
-	Movies interface {
-		Insert(movie *Movie) error
-		Update(movie *Movie) error
-		Delete(id int64) error
-		Get(id int64) (*Movie, error)
-	}
+	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModel(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
-
 }
 
 // for unit test models
+/*
 func NewMockModel() Models {
 	return Models{
 		Movies: MockMovieModel{},
 	}
 }
+*/
