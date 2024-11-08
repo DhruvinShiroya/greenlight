bad93643ec0acb575d862b278fc8b53ca974b2ea
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-11-08 01:00:39 -0500

    update movie method for better db error handling

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -22,7 +22,6 @@ type Movie struct {
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
-
 	// use check method to perform validation check
 	v.Check(movie.Title != "", "title", "must be provided")
 	v.Check(len(movie.Title) <= 500, "title", "title must not be more than 500 bytes long")
@@ -38,7 +37,6 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) <= 5, "genres", "must have less than 5 genres")
 	// check if all the genres are unique or not
 	v.Check(validator.Unique(movie.Genres), "genres", "must contain unique genres")
-
 }
 
 // define movie model
@@ -57,13 +55,12 @@ func (m MovieModel) Insert(movie *Movie) error {
 	// create an args slice for the placeholder paramter
 	// define empty slice interface and define slice immediately next to our SQL query
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-	//because our query returns the value use the method QueryRow() otherwise use Exec() command to execute query
+	// because our query returns the value use the method QueryRow() otherwise use Exec() command to execute query
 	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // add placeholder method for getting the record from movie table
 func (m MovieModel) Get(id int64) (*Movie, error) {
-
 	// check if id is valid positive
 	// postgres bigserial starts form 1 auto-increment
 	if id < 1 {
@@ -77,7 +74,6 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	// use pq.QueryRow() method for executing query, scan the response data into field of Movie struct
 	// if getting array from result, we need to convert it to pq.Array(arr) to scan target for the genres
 	err := m.DB.QueryRow(query, id).Scan(&movie.ID, &movie.CreatedAt, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version)
-
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -89,56 +85,64 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 	// if not error return movie struct
 	return &movie, nil
-
 }
 
 // add method for update the record in the movie table
 func (m MovieModel) Delete(id int64) error {
-  // check if the id is positive 
-  if id < 1 {
-    return ErrRecordNotFound
-  }
-  // construct qeury to delete the movie with id
-  qeury := `DELETE FROM movies WHERE id = $1`
+	// check if the id is positive
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	// construct qeury to delete the movie with id
+	qeury := `DELETE FROM movies WHERE id = $1`
 
-  // since the we are not returning anything we can use exec() method 
-  // which will return result.rowsaffected() which contains information about how many 
-  // rows has been affected , if 0 rows affectd means that movies with id
-  // coulnd not be found and if found it will return 1 in  result.rowsaffected()
-  result , err := m.DB.Exec(qeury,id)
-  if err != nil{
-    return err
-  }
-  rowsaffected , err :=  result.RowsAffected() 
-  if err != nil {
-    return err
-  }
-  if rowsaffected == 0 {
-    return ErrRecordNotFound
-  }
-  // otherwise result sould be one since we are using primary key to delete record
-  // return nil upon movie delete
+	// since the we are not returning anything we can use exec() method
+	// which will return result.rowsaffected() which contains information about how many
+	// rows has been affected , if 0 rows affectd means that movies with id
+	// coulnd not be found and if found it will return 1 in  result.rowsaffected()
+	result, err := m.DB.Exec(qeury, id)
+	if err != nil {
+		return err
+	}
+	rowsaffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsaffected == 0 {
+		return ErrRecordNotFound
+	}
+	// otherwise result sould be one since we are using primary key to delete record
+	// return nil upon movie delete
 	return nil
 }
 
 // add method to update the record in the movie table
 func (m MovieModel) Update(movie *Movie) error {
-  // write update movie query  for title, runtime, genres, year
-  // also update the version with each update
-  query := `
+	// write update movie query  for title, runtime, genres, year
+	// also update the version with each update
+	query := `
        UPDATE movies
        SET title = $1, year = $2, runtime = $3, genres = $4, version= version + 1
        WHERE id= $5
        RETURNING version`
 
-  args := []interface{}{
-    movie.Title,
-    movie.Year,
-    movie.Runtime,
-    pq.Array(movie.Genres),
-    movie.ID,
-  }
-  return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 // mock movie struct for unit testing
